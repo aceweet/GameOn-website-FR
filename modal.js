c611ca883afc5a7e6f36ae08c9f84d048e36bfd9
aceweet <andresweetman@gmail.com>
2021-12-08 15:30:20 +0100

    js fix

--- modal.js
+++ modal.js
@@ -1,3 +1,6 @@
+const FORM_DATA = [{ key: "toto", state: "default", message: undefined }];
+const ERROR_DATA = [{ key: "toto", message: "titi" }];
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -14,11 +17,11 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const modalCloseBtn = document.querySelector(".close");
 const formData = document.querySelectorAll(".formData");
-console.log("modal-btn", modalCloseBtn)
+console.log("modal-btn", modalCloseBtn);
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-modalCloseBtn.addEventListener("click", alert("andre"))
+modalCloseBtn.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
@@ -28,15 +31,16 @@ function launchModal() {
 // close modal form
 function closeModal() {
   modalbg.style.display = "none";
+  // alert("andre")
 }
+
+// check form conditions thru array
 
 // document.getElementById("closeform").addEventListener("click", function(closeModal) {
 // 	modalBg.style.display = "none";
 // });
 
-
 // // fermer formulaire via croix
 // document.getElementsById("#close").addEventListener("click", function(closeModal) {
 // 	modalBg.style.display = "none";
 // });
-
